17bba41dd6ee3aac646538e73855b946e1c6d2d1
Mats Johansson <matjoh50@gmail.com>
2023-09-27 20:55:30 +0200

    update everything

--- main.js
+++ main.js
@@ -1,13 +1,11 @@
-alternateSpanPossition();
 
 function alternateSpanPossition() {
   // Select all elements with the class "span-poss"
-  var elements = document.getElementsByClassName("span-poss");
+  let elements = document.getElementsByClassName("span-poss");
   try {
     while (0 < elements.length) {
       if (elements.length % 2 == 0) {
         // elements get "span-left" class
-        // bool = false;
         elements[0].classList.add("left", "base");
         elements[0].classList.remove("span-poss");
       } else {
